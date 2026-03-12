ce1d42d07518595850de33d9704f62f7612e420d
JasperJaspe <j.gruppelaar@student.maastrichtuniversity.nl>
2026-03-12 20:42:11 +0100

    Add files via upload

---
+++ three_species_s1.m
@@ -0,0 +1,31 @@
+t0 = 0;                 	% start time of simulation [in months]
+tfinal = 200;               % end time of simulation [in months]
+tspan = [t0 tfinal];        % time span defined by the start and end times of the simulation [in months]
+a1 = 3.25;                  % hare hunting plant
+a2 = 0.3;                   % lynx hunting hare
+b1 = 7.69;                  % hare eating efficiency
+b2 = 3.07;                  % lynx eating efficiency
+d1 = 0.195;                 % hare natural death rate
+d2 = 0.0325;                % lynx natural death rate
+y0 = [0.8 0.5 0.2];       
+              
+for i = 1:1
+
+[t,y] = ode45(@(t,y) plants_hare_lynx(t,y,a1,a2,b1,b2,d1(i),d2), tspan, y0);
+
+% plotting the population over time
+figure("Name",'Plant, Hare & Lynx')
+plot(t,y)
+title('Plants/Hares/Lynxes Populations Over Time')
+xlabel('Time [months]')
+ylabel('Population')
+legend('Plant','Hare', 'Lynx', 'Location','North')
+
+% plotting the state space
+figure("Name",'State space')
+plot3(y(:,1),y(:,2),y(:,3))
+title('State space')
+xlabel('Plant Population')
+ylabel('Hare Population')
+zlabel('Lynx Population')
+end
